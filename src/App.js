1c007ab1e7f3443910ca4b07c1ec1f48204e39aa
rossxhunter <ross98hunter@gmail.com>
2022-01-03 19:53:27 +0000

    make functional

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Home } from "./pages/Home";
+import { createTheme, CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
 
 function App() {
-  return <Home />;
+  const theme = createTheme({
+    palette: {
+      primary: { main: "#333" },
+    },
+    typography: {
+      fontFamily: "Fira Code, Arial",
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,46 @@
+import { Download, GitHub, LinkedIn } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+
 export const Home = () => {
-  return <div>ross.</div>;
+  return (
+    <Stack
+      spacing={2}
+      direction="column"
+      alignItems="center"
+      justifyContent="space-between"
+      sx={{ height: "99vh", paddingBottom: "0px" }}
+    >
+      <Box />
+      <Stack alignItems="center" direction="column" spacing={2}>
+        <Typography variant="h2">ross hunter</Typography>
+        <Button
+          download
+          href="/files/cv.pdf"
+          variant="outlined"
+          startIcon={<Download />}
+        >
+          <Typography variant="h6">cv</Typography>
+        </Button>
+      </Stack>
+      <Stack direction="row" spacing={1}>
+        <IconButton target="_blank" href="https://github.com/rossxhunter">
+          <GitHub />
+        </IconButton>
+        <IconButton
+          target="_blank"
+          href="https://www.linkedin.com/in/rossxhunter/"
+        >
+          <LinkedIn />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
 };
